79a133815793299efd591f076023fafb4a560f0a
LianggaRistiana <liangga4@gmail.com>
2025-05-07 00:29:12 +0800

    fix: supress hydration warn

--- components/provider/theme-provider.tsx
+++ components/provider/theme-provider.tsx
@@ -1,27 +1,3 @@
-// "use client";
-
-// import * as React from "react";
-// import { ThemeProvider as NextThemesProvider } from "next-themes";
-
-// import { useState, useEffect } from "react";
-
-// export function ThemeProvider({
-//   children,
-//   ...props
-// }: React.ComponentProps<typeof NextThemesProvider>) {
-//   const [isClient, setIsClient] = useState(false);
-
-//   useEffect(() => {
-//     setIsClient(true);
-//   }, []);
-
-//   if (!isClient) {
-//     return <>{children}</>;
-//   }
-//   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-// }
-
-
 "use client"
 
 import * as React from "react"

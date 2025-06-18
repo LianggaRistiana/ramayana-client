8282fea010b79715c43f7913da1ece14e9083d13
LianggaRistiana <liangga4@gmail.com>
2025-06-18 22:35:54 +0800

    feat: component response can read markdown format

--- components/atoms/navbar.tsx
+++ components/atoms/navbar.tsx
@@ -2,15 +2,12 @@ import { ModeToggle } from "./mode-toggle";
 
 
 export default function Navbar() {
-
-
     return (
         <nav className="fixed top-0 w-full gap-4 z-50 flex items-center justify-between py-4 px-4 lg:px-12 xl:px-24 backdrop-blur-lg bg-background/80 border-b ">
             <p  className="text-xl font-bold text-primary">
                 Ramayana
             </p>
             <ModeToggle></ModeToggle>
-            
         </nav>
     );
 
--- components/atoms/response.tsx
+++ components/atoms/response.tsx
@@ -1,8 +1,13 @@
+import ReactMarkdown from 'react-markdown';
+
 export default function Response({ children }: { children: string }) {
     return (
         <div className="w-full pb-4">
-            <div className=" whitespace-pre-line w-fit">
+            {/* <div className=" whitespace-pre-line w-fit">
                 {children}
+            </div> */}
+            <div className="prose dark:prose-invert max-w-none">
+                <ReactMarkdown>{children}</ReactMarkdown>
             </div>
         </div>
     );
